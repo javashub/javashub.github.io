e10fddf1657407809285d9d4caf5b0d09a815e97
JavasHub <imlyk@qq.com>
2025-03-15 21:45:28 +0800

    update

--- docs/.vuepress/navbar.js
+++ docs/.vuepress/navbar.js
@@ -11,14 +11,14 @@ export const zhNavbar = defineNavbarConfig([
   },
 ])
 
-export const enNavbar = defineNavbarConfig([
-  { text: 'Home', link: '/en/' },
-  { text: 'Blog', link: '/en/blog/' },
-  { text: 'Tags', link: '/en/blog/tags/' },
-  { text: 'Archives', link: '/en/blog/archives/' },
-  {
-    text: 'Notes',
-    items: [{ text: 'Demo', link: '/en/notes/demo/README.md' }]
-  },
-])
+// export const enNavbar = defineNavbarConfig([
+//   { text: 'Home', link: '/en/' },
+//   { text: 'Blog', link: '/en/blog/' },
+//   { text: 'Tags', link: '/en/blog/tags/' },
+//   { text: 'Archives', link: '/en/blog/archives/' },
+//   {
+//     text: 'Notes',
+//     items: [{ text: 'Demo', link: '/en/notes/demo/README.md' }]
+//   },
+// ])
 
