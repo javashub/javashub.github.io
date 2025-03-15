f51d24048357d70cb194bab374e51bd1aa6a5c13
JavasHub <imlyk@qq.com>
2025-03-15 22:08:45 +0800

    update

--- docs/.vuepress/navbar.js
+++ docs/.vuepress/navbar.js
@@ -4,7 +4,7 @@ export const zhNavbar = defineNavbarConfig([
   { text: '首页', link: '/' },
   { text: '博客', link: '/blog/' },
   { text: '标签', link: '/blog/tags/' },
-  { text: '归档', link: '/blog/archives/' },
+  // { text: '归档', link: '/blog/archives/' },
   {
     text: '笔记',
     items: [{ text: '示例', link: '/notes/demo/README.md' }]
