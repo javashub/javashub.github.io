5138d0d5bd10879c614ac6adf80af2e5350546a0
JavasHub <imlyk@qq.com>
2025-03-25 00:04:39 +0800

    update theme

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -5,6 +5,7 @@ import theme from "./theme.js";
 
 
 export default defineUserConfig({
+
     base: "/",
 
     lang: "zh-CN",
@@ -12,6 +13,18 @@ export default defineUserConfig({
     description: "描述信息",
 
     theme,
+
+    // index.html中注入 head  参考：https://theme-hope.vuejs.press/zh/guide/customize/font.html#%E4%BA%BA%E4%BA%BA%E7%94%9F%E8%80%8C%E8%87%AA%E7%94%B1
+    head: [
+        [
+            `link`,
+            { rel: `stylesheet` , href: `https://cdn.jsdelivr.net/npm/@docsearch/css@3`}
+        ],
+        [
+            `script`,
+            { src: `https://cdn.jsdelivr.net/npm/@docsearch/js@3`}
+        ]
+    ],
 
     // 对更低版本的浏览器提供支持
     extendsBundlerOptions: (config, app) => {
@@ -24,17 +37,7 @@ export default defineUserConfig({
         });
     },
 
-    // index.html中注入 head  参考：https://theme-hope.vuejs.press/zh/guide/customize/font.html#%E4%BA%BA%E4%BA%BA%E7%94%9F%E8%80%8C%E8%87%AA%E7%94%B1
-    head: [
-        [
-            "link",
-            { rel: "stylesheet" , href: "https://cdn.jsdelivr.net/npm/@docsearch/css@3"}
-        ],
-        [
-            "script",
-            { src: "https://cdn.jsdelivr.net/npm/@docsearch/js@3"}
-        ]
-    ]
+
 
     // 和 PWA 一起启用
     // shouldPrefetch: false,
