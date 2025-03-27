5a8263d755fc7dbeffedb1cff1ff26abb3d79a00
JavasHub <imlyk@qq.com>
2025-03-27 23:38:16 +0800

    update theme

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -50,6 +50,10 @@ export default hopeTheme({
   metaLocales: {
     // editLink: "在 GitHub 上编辑此页",
   },
+  // 编辑此页链接
+  editLink: false,
+  // 页面贡献者
+  contributors: false,
 
   // 如果想要实时查看任何改变，启用它。注: 这对更新性能有很大负面影响
   // hotReload: true,
